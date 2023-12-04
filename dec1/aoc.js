5a31ba17228cabc333a9f39a1d543d0e9c5db2ad
Eoin Halligan <eoin@losant.com>
2023-12-04 17:21:04 -0500

    day 1 solutions

--- dec1/aoc.js
+++ dec1/aoc.js
@@ -1,7 +1,32 @@
 import Wreath from "../wreath.js";
 
-const part1 = (data) => {
-    console.log('here iot is!!!', data)
+const part1 = (fn) => {
+    const wreath = new Wreath('dec1');
+    let data = 0;
+    wreath.onEachLine(line => {
+        const [first] = line.split('').flat().filter(char => !isNaN(Number(char)));
+        const [last] = line.split('').flat().reverse().filter(char => !isNaN(Number(char)));
+        data += Number(String.prototype.concat.call(first, last))
+    });
+    wreath.endOfFile(() => fn(data));
 }
 
-const dec1 = new Wreath('dec1', part1)
+const part2 = (fn) => {
+    const wreath = new Wreath('dec1');
+    let data = 0;
+    const tokens = {
+        'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, /* noine */
+    }
+
+    wreath.onEachLine(line => {
+        line = line.replaceAll(/(?=(one|two|three|four|five|six|seven|eight|nine))/g, (text, match) => (tokens[match]));
+        const [first] = line.split('').flat().filter(char => !isNaN(Number(char)));
+        const [last] = line.split('').flat().reverse().filter(char => !isNaN(Number(char)));
+        data += Number(String.prototype.concat.call(first, last))
+    });
+    wreath.endOfFile(() => fn(data));
+}
+
+
+part1(calibrationValue => console.info(`Part 1: ${calibrationValue}`)); // 54390
+part2(calibrationValue => console.info(`Part 2: ${calibrationValue}`)); // 54277
--- wreath.js
+++ wreath.js
@@ -3,10 +3,18 @@ import readline from 'readline';
 
 export default class Wreath {
 
-    constructor(dir, lineHandler, resultHandler) {
+
+    constructor(dir) {
         this.input = fs.createReadStream(`./../${dir}/input.txt`, 'utf-8');
         this.reader = readline.createInterface({ input: this.input });
-        this.reader.on('line', (line) => (lineHandler(line)))
-        
     }
+
+    onEachLine(fn) {
+        this.reader.on('line', fn)
+    }
+
+    endOfFile(fn) {
+        this.reader.on('close', fn)
+    }
+
 }
